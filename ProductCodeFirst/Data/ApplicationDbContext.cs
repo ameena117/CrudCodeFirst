2ce8d121b56bd7f60b3358cdf1ec5c8be0a62ef6
ameena117 <ameenajadallah@gmail.com>
2024-12-04 23:03:16 +0200

    Add project files.

---
+++ ProductCodeFirst/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCodeFirst.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;database=Product;Trusted_Connection=true;TrustServerCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ProductCodeFirst/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductCodeFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
